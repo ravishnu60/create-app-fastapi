ae0c572caeebb65e4b4a32c8f160e589ee7a7b53
ravishnu60 <ravishnu60@gmail.com>
2024-04-27 16:04:01 +0530

    as package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup( 
+	name="create-app-fastapi", 
+	version="0.0.3", 
+	author="Ravishnu", 
+	author_email="ravishnu60@gmail.com", 
+	packages=find_packages(), 
+	description="Package to create fastapi project", 
+	long_description="A package used to create fastAPI project structure with virtual environment and dependencies.", 
+	long_description_content_type="text/markdown", 
+	python_requires='>=3.9', 
+	install_requires=[],
+	entry_points ={ 
+		'console_scripts': [ 
+			'create-app-fastapi=projectfastapi.main:main'
+		] 
+	},
+    keywords=['fastapi', 'fastapi project','project structure','api structure', 'python fastapi project structure']
+) 
